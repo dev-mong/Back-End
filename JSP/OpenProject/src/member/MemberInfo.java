34ad438fd60b6ae851465119a6f6b400c916131c
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-07-03 16:45:11 +0900

    memer 정보 저장 객체
    setter 삭제 : 로그인 정보 변경 불가

--- JSP/OpenProject/src/member/MemberInfo.java
+++ JSP/OpenProject/src/member/MemberInfo.java
@@ -3,31 +3,30 @@ package member;
 public class MemberInfo {
 	private String id;
 	private String pw;
-
-	public MemberInfo(String id, String pw) {
+	private String chk;
+	public MemberInfo(String id, String pw, String chk) {
 		super();
 		this.id = id;
 		this.pw = pw;
-
+		this.chk = chk;
 	}
 	public String getId() {
 		return id;
 	}
-	public void setId(String id) {
-		this.id = id;
-	}
+	
 	public String getPw() {
 		return pw;
 	}
-	public void setPw(String pw) {
-		this.pw = pw;
+	
+	public String getChk() {
+		return chk;
 	}
+	
 	@Override
 	public String toString() {
-		return "MemberInfo [id=" + id + ", pw=" + pw + "]";
+		return "MemberInfo [id=" + id + ", pw=" + pw + ", chk=" + chk + "]";
 	}
-
-
-}
 	
 
+	
+}
