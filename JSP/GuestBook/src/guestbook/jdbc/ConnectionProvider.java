816c168057fde75bc365c38b57c6488b4bea2706
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-07-08 16:02:38 +0900

    Connection 생성

---
+++ JSP/GuestBook/src/guestbook/jdbc/ConnectionProvider.java
@@ -0,0 +1,27 @@
+package guestbook.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionProvider {
+
+	
+	public static Connection getConnection() throws SQLException {
+	
+		String jdbcUrl="jdbc:apache:commons:dbcp:pool";
+		
+		//DB Connection 정보 
+		//String dbUrl="jdbc:oracle:thin:@localhost:1522:orcl";
+		//String user="scott";
+		//String pw="tiger";
+		
+		//return DriverManager.getConnection(dbUrl,user,pw);
+	
+		return DriverManager.getConnection(jdbcUrl);
+	}
+	
+	
+}
