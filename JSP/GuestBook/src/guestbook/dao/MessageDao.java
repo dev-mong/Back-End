8a8bb50d6b88bf544e6f1abcf13742c7adb91547
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-07-09 16:23:52 +0900

    Delete 리스트

--- JSP/GuestBook/src/guestbook/dao/MessageDao.java
+++ JSP/GuestBook/src/guestbook/dao/MessageDao.java
@@ -55,7 +55,7 @@ public class MessageDao {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 
-		//list 객체 생성
+		// list 객체 생성
 		List<Message> list = new ArrayList<>();
 
 		try {
@@ -94,6 +94,7 @@ public class MessageDao {
 		return list;
 	}
 
+	// 페이지 Count
 	public int selectTotalCount(Connection conn) throws SQLException {
 
 		int resultCnt = 0;
@@ -106,22 +107,80 @@ public class MessageDao {
 			stmt = conn.createStatement();
 
 			String sql = "select count(*) from guestbook_message";
-			
-			rs=stmt.executeQuery(sql);
-			
-			if(rs.next()) {
+
+			rs = stmt.executeQuery(sql);
+
+			if (rs.next()) {
 				resultCnt = rs.getInt(1);
 			}
-		
+
 		} finally {
-			if(rs != null) {
+			if (rs != null) {
 				rs.close();
 			}
-			if(stmt != null) {
+			if (stmt != null) {
 				stmt.close();
 			}
 		}
 
+		return resultCnt;
+	}
+
+	// 데이터 삭제를 위한 검색
+	public Message selectMessage(Connection conn, int mid) throws SQLException {
+
+		Message message = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+			String sql = "select * from guestbook_message where message_id=?";
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setInt(1, mid);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				message = new Message(rs.getInt("message_id"), rs.getString("guest_name"), rs.getString("password"),
+						rs.getString("message"));
+			}
+
+		} finally {
+
+			if (rs != null) {
+				rs.close();
+			}
+
+			if (pstmt != null) {
+				pstmt.close();
+			}
+
+		}
+
+		return message;
+	}
+
+	public int deleteMessage(Connection conn, int mid) throws SQLException {
+
+		int resultCnt = 0;
+
+		PreparedStatement pstmt = null;
+
+		String sql = "delete from guestbook_message where message_id=?";
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, mid);
+
+			resultCnt = pstmt.executeUpdate();
+
+		} finally {
+			if (pstmt != null) {
+				pstmt.close();
+			}
+		}
 		return resultCnt;
 	}
 
