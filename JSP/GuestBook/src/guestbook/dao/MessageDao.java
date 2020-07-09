0fc70400675bbee3441f52f713af197a0f28f735
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-07-09 13:21:30 +0900

    DAO

--- JSP/GuestBook/src/guestbook/dao/MessageDao.java
+++ JSP/GuestBook/src/guestbook/dao/MessageDao.java
@@ -2,47 +2,127 @@ package guestbook.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import guestbook.model.Message;
 
 public class MessageDao {
 
-	//싱글톤 
+	// 싱글톤
 	private MessageDao() {
 	}
 
-	static private MessageDao dao=new MessageDao();
-	
+	static private MessageDao dao = new MessageDao();
+
 	public static MessageDao getInstance() {
 		return dao;
 	}
 
+	// 데이터 저장
 	public int insertMessage(Connection conn, Message message) throws SQLException {
 
-		int resultCnt=0;
-		
+		int resultCnt = 0;
+
 		PreparedStatement pstmt = null;
 		try {
-			
-			String sql ="insert into guestbook_message values (message_id_seq.nextVal,?,?,?)";
+
+			String sql = "insert into guestbook_message values (message_id_seq.nextVal,?,?,?)";
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setString(1, message.getUname());
 			pstmt.setString(2, message.getPw());
 			pstmt.setString(3, message.getMessage());
-			
+
 			resultCnt = pstmt.executeUpdate();
-			
-		}finally {
-			if(pstmt !=null) {
-				pstmt.close();	
+
+		} finally {
+			if (pstmt != null) {
+				pstmt.close();
 			}
-			
+
 		}
-		
+
 		return resultCnt;
 	}
-	
+
+	// 데이터 출력
+	public List<Message> selectMesseageList(Connection conn, int startRow, int endRow) throws SQLException {
+
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		//list 객체 생성
+		List<Message> list = new ArrayList<>();
+
+		try {
+
+			String sql = "select message_id, guest_name, password, message " + "from ("
+					+ "select  rownum rnum,message_id, guest_name, password, message " + "	from("
+					+ "		select * from guestbook_message m order by m.message_id desc " + "	) where rownum<=?"
+					+ ") where rnum>=?";
+
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setInt(1, endRow);
+			pstmt.setInt(2, startRow);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				Message message = new Message(rs.getInt("message_id"), rs.getString("guest_name"),
+						rs.getString("password"), rs.getString("message"));
+
+				list.add(message);
+			}
+
+		} finally {
+
+			if (rs != null) {
+				rs.close();
+			}
+
+			if (pstmt != null) {
+				pstmt.close();
+			}
+
+		}
+
+		return list;
+	}
+
+	public int selectTotalCount(Connection conn) throws SQLException {
+
+		int resultCnt = 0;
+
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		try {
+
+			stmt = conn.createStatement();
+
+			String sql = "select count(*) from guestbook_message";
+			
+			rs=stmt.executeQuery(sql);
+			
+			if(rs.next()) {
+				resultCnt = rs.getInt(1);
+			}
+		
+		} finally {
+			if(rs != null) {
+				rs.close();
+			}
+			if(stmt != null) {
+				stmt.close();
+			}
+		}
+
+		return resultCnt;
+	}
+
 }
