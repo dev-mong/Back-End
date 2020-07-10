370e269c4023be17b10d557f7951b39982e30f4e
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-07-10 17:05:31 +0900

    리포트 DB SQL

---
+++ GuestDB/report create table.sql
@@ -0,0 +1,12 @@
+--report
+
+create table report(
+    idx number(4) PRIMARY KEY,
+    sname varchar2(20) not null,
+    sno varchar2(10) not null,
+    reportfile varchar2(100) not null
+);
+
+create sequence report_seq;
+
+insert into report values (report_seq.nextval, "홍길동", "1234","/file/123412341234_report.ppt");
